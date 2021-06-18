4859a02227d70243ffed01706ed1926642a99436
yama <tkshyamaguchi45@gmail.com>
2021-06-18 19:49:18 +0900

    style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,9 @@ module.exports = {
         test: /\.css/,
         use: [
           {
+            loader: 'style-loader'
+          },
+          {
             loader: 'css-loader',
           },
         ],
