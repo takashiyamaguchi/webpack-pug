9a91649caf77b6e0d9a77a8524173ed9387a292d
yama <tkshyamaguchi45@gmail.com>
2021-06-18 19:40:56 +0900

    css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,18 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-  }
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
 }
